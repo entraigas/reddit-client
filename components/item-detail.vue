202dfc606db5cca7bafcc74e425bcf13900977a6
Marcelo Entraigas <entraigas@gmail.com>
2019-10-01 00:19:12 -0300

    Refactor code. Add components.

---
+++ components/item-detail.vue
@@ -0,0 +1,26 @@
+<template>
+  <div v-if="showDetails">
+    <h3>{{ selected.data.author }}</h3>
+    <img :src="selected.data.thumbnail" />
+    <p>{{ selected.data.title }}</p>
+  </div>
+</template>
+
+<script>
+import { isEmpty } from 'lodash'
+
+export default {
+  name: 'ItemDetail',
+  props: {
+    selected: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  computed: {
+    showDetails() {
+      return !isEmpty(this.selected)
+    }
+  }
+}
+</script>
---
+++ pages/index copy.vue
@@ -0,0 +1,138 @@
+<template>
+  <div>
+    <div class="menu">
+      <ol class="list">
+        <li
+          v-for="item in news"
+          :key="item.data.id"
+          :class="{ removed: removed.includes(item.data.id) }"
+          @click="selected = item"
+        >
+          <div>
+            <span class="item-author">{{ item.data.author }}</span>
+            {{ getTimeDiff(item.data.created_utc) }}
+          </div>
+          <img :src="item.data.thumbnail" />
+          <div class="item-text">
+            {{ item.data.title }}
+          </div>
+          <div class="item-footer">
+            <span @click="removeItem(item.data.id)">Dismiss post</span>
+            <div class="item-comment">
+              {{ item.data.num_comments }} comments
+            </div>
+          </div>
+        </li>
+      </ol>
+    </div>
+    <div class="detail">selected = {{ selected }}</div>
+  </div>
+</template>
+
+<script>
+import { debounce } from 'lodash'
+import { mapGetters } from 'vuex'
+
+export default {
+  data() {
+    const now = Math.round(new Date().getTime() / 1000)
+    return {
+      now,
+      removed: [],
+      selected: null
+    }
+  },
+  computed: {
+    ...mapGetters({
+      news: 'news/news'
+    })
+  },
+  methods: {
+    getTimeDiff(time) {
+      const minutes = Math.floor((this.now - time) / 60)
+      if (minutes < 45) {
+        return minutes < 5 ? 'now' : +`${minutes} minutes ago`
+      }
+      const hours = Math.ceil(minutes / 60)
+      if (hours < 22) {
+        return hours === 1 ? 'an hour ago' : `${hours} hours ago`
+      }
+      const days = Math.ceil(hours / 24)
+      return days === 1 ? 'a day ago' : `${days} days ago`
+    },
+    removeItem(id) {
+      this.removed.push(id)
+      this.debouncedRemove(id)
+    },
+    debouncedRemove: debounce(function(id) {
+      this.$store.commit('news/removeItemById', id)
+    }, 1000)
+  }
+}
+</script>
+
+<style>
+.detail {
+  margin-left: 35%;
+  padding-left: 2em;
+  padding-top: 2em;
+  max-width: 65%;
+}
+.menu {
+  position: fixed;
+  top: 0;
+  left: 0;
+  max-width: 35%;
+  max-height: 100%;
+  height: 100%;
+  overflow-y: scroll !important;
+  background-color: black;
+  color: white;
+}
+.menu .list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  height: 100%;
+}
+.menu li {
+  position: relative;
+  border-bottom: 1px solid #363537;
+  padding: 0.5em;
+  height: 11em;
+  transition: transform 1s;
+}
+.menu li.removed {
+  transform: translateX(-100%);
+}
+.menu .item-author {
+  font-size: 1.3em;
+  font-weight: 600;
+  padding-left: 0.5em;
+}
+.menu .item-text {
+  height: 6em;
+  padding-left: 0.5em;
+  overflow-y: hidden;
+}
+.menu img {
+  width: 6em;
+  height: 6em;
+  min-width: 6em;
+  min-height: 6em;
+  float: left;
+}
+.menu .item-footer {
+  margin-top: 0.5em;
+  font-weight: 600;
+}
+.menu .item-footer span {
+  cursor: pointer;
+}
+.menu .item-comment {
+  float: right;
+  margin-right: 1em;
+  font-weight: 500;
+  color: #8b6847;
+}
+</style>
